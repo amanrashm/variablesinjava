f6b39b46430d21fb8634591cb74f18260c15d0d2
amanrashm <amanra664@gmail.com>
2022-05-12 22:05:40 +0530

    Initial commit

---
+++ src/com/aman/Variables.java
@@ -0,0 +1,20 @@
+package com.aman;
+import java.util.Scanner;
+public class Variables {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        /*types of variable java supports
+                instance variables : can be created in each object
+               static variable : can be created once in memory
+                local variable : can be created inside each method
+                 no global concept in java
+                        */
+
+
+
+
+    }
+}
---
+++ src/com/aman/student.java
@@ -0,0 +1,17 @@
+package com.aman;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+public class student {
+
+    int sid;
+    String name;
+    int phonenumber;
+    static String collegename = "aman"
+//    void main{}
+    Scanner x = new Scanner (System.in);
+    
+
+
+
+}
